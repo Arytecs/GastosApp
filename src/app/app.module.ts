96e813cf5f0cefbabb463691cf6327ea38b776a8
victor <victorcm34@gmail.com>
2018-07-19 21:23:59 +0200

    Login to menu
    El login ya va al menú y este carga el HOME anque sin ROUTERS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './menu/home/home.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  {path: '', component:LoginComponent},
+  {
+    path: 'menu', 
+    component:MenuComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit() {
+    $(".push_menu").click(function(){
+      $(".wrapper").toggleClass("active");
+ });
+  }
+
+}
